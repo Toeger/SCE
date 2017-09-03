96c9ef6d7f5d1f6e48c616011515f08d5c29a6e3
toeger <toeger@anonymouse.me>
2017-09-03 14:27:12 +0200

    Opening files and closing tabs.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFile>
+#include <QFileDialog>
+#include <QPlainTextEdit>
+
 MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow{parent}
 	, ui{std::make_unique<Ui::MainWindow>()} {
@@ -8,3 +12,21 @@ MainWindow::MainWindow(QWidget *parent)
 }
 
 MainWindow::~MainWindow() {} //required for destructors of otherwise incomplete type Ui::MainWindow
+
+void MainWindow::on_actionOpen_File_triggered() {
+	for (const auto &file_name : QFileDialog::getOpenFileNames(this, tr("Select File(s) to open"))) {
+		auto file_edit = std::make_unique<QPlainTextEdit>();
+		QFile file{file_name};
+		file.open(QFile::ReadOnly);
+		if (file.isOpen()) {
+			file_edit->setPlainText(file.readAll());
+		} else {
+			file_edit->setPlaceholderText(tr("Failed reading file %1").arg(file_name));
+		}
+		ui->file_tabs->addTab(file_edit.release(), file_name);
+	}
+}
+
+void MainWindow::on_file_tabs_tabCloseRequested(int index) {
+	ui->file_tabs->removeTab(index);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,13 @@ class MainWindow : public QMainWindow {
 	explicit MainWindow(QWidget *parent = 0);
 	~MainWindow();
 
+	private slots:
+	void on_actionOpen_File_triggered();
+	void on_file_tabs_tabCloseRequested(int index);
+
 	private:
 	std::unique_ptr<Ui::MainWindow> ui;
+	Ui::MainWindow *_; //Qt Designer only works correctly if it finds this string
 };
 
 #endif // MAINWINDOW_H
