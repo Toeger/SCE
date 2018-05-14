86d4da6de2d788f35e646002cb6453e6e28d052d
toeger <toeger@anonymouse.me>
2018-05-14 18:17:13 +0200

    Implemented basic popup notes for edit window.

--- ui/edit_window.cpp
+++ ui/edit_window.cpp
@@ -5,7 +5,11 @@
 #include "logic/tool_actions.h"
 
 #include <QAction>
+#include <QColor>
+#include <QEvent>
 #include <QMessageBox>
+#include <QToolTip>
+#include <QWheelEvent>
 #include <memory>
 
 Edit_window::Edit_window() {
@@ -13,10 +17,29 @@ Edit_window::Edit_window() {
 	highlighter->load_rules(TEST_DATA_PATH "c++-syntax.json");
 	syntax_highlighter = std::move(highlighter);
 	Tool_actions::add_widget(this);
+	connect(this, &QPlainTextEdit::textChanged, [this] { state++; });
 }
 
 Edit_window::~Edit_window() {
 	Tool_actions::remove_widget(this);
+}
+
+void Edit_window::add_note(Edit_window::Note note) {
+	QTextCursor tc{textCursor()};
+	QTextCharFormat tcf;
+	tcf.setUnderlineStyle(QTextCharFormat::UnderlineStyle::WaveUnderline);
+	tcf.setUnderlineColor(note.color);
+	tc.setCharFormat(tcf);
+
+	notes.push_back(std::move(note));
+}
+
+void Edit_window::clear_notes() {
+	notes.clear();
+}
+
+uint32_t Edit_window::get_state() const {
+	return state;
 }
 
 void Edit_window::wheelEvent(QWheelEvent *we) {
@@ -29,4 +52,26 @@ void Edit_window::wheelEvent(QWheelEvent *we) {
 	} else {
 		QPlainTextEdit::wheelEvent(we);
 	}
+}
+
+bool Edit_window::event(QEvent *event) {
+	if (event->type() == QEvent::ToolTip) {
+		QHelpEvent *helpEvent = static_cast<QHelpEvent *>(event);
+		QTextCursor cursor = cursorForPosition(helpEvent->pos());
+		const int character = cursor.positionInBlock();
+		const int line = cursor.blockNumber();
+		if (line == 0) {
+			QToolTip::hideText();
+			return true;
+		}
+		for (const auto &note : notes) {
+			if (note.line == line && note.char_start < character && note.char_end > character) {
+				QToolTip::showText(helpEvent->globalPos(), note.text);
+				return true;
+			}
+		}
+		QToolTip::hideText();
+		return true;
+	}
+	return QPlainTextEdit::event(event);
 }
--- ui/edit_window.h
+++ ui/edit_window.h
@@ -8,7 +8,9 @@
 #include <vector>
 
 class QAction;
+class QEvent;
 class QSyntaxHighlighter;
+class QWheelEvent;
 
 //Widget for code editing
 class Edit_window : public QPlainTextEdit {
@@ -17,13 +19,27 @@ class Edit_window : public QPlainTextEdit {
 	Edit_window();
 	~Edit_window();
 
+	struct Note {
+		QString text;
+		QColor color;
+		int line;
+		int char_start;
+		int char_end;
+	};
+	void add_note(Note note);
+	void clear_notes();
+	uint32_t get_state() const;
+
 	private:
 	void wheelEvent(QWheelEvent *we) override;
+	bool event(QEvent *event) override;
 	void show_output(const QString &output, Tool_output_target::Type output_target, const QString &title, bool is_error);
 
 	int zoom_remainder{};
 	std::vector<std::unique_ptr<QAction>> actions;
 	std::unique_ptr<QSyntaxHighlighter> syntax_highlighter;
+	std::vector<Note> notes;
+	uint32_t state{};
 };
 
 #endif // EDIT_WINDOW_H
