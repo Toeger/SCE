407197480b3ec2920fc900ae9cb03e937bbc9811
toeger <toeger@anonymouse.me>
2018-06-18 13:57:46 +0200

    Fixed shadowing issue.

--- utility/thread_check.h
+++ utility/thread_check.h
@@ -5,18 +5,18 @@
 #include <thread>
 
 struct Thread_check {
-    Thread_check()
-        : id{std::this_thread::get_id()} {}
-    //set the current thread as the one that will be ckecked for in thread_check
-    void set_thread() {
-        id = std::this_thread::get_id();
-    }
-    //checks if the function was called by the same thread that called set_thread
-    void thread_check() const {
-        assert(id == std::this_thread::get_id());
-    }
+	Thread_check()
+		: thread_id{std::this_thread::get_id()} {}
+	//set the current thread as the one that will be ckecked for in thread_check
+	void set_thread() {
+		thread_id = std::this_thread::get_id();
+	}
+	//checks if the function was called by the same thread that called set_thread
+	void thread_check() const {
+		assert(thread_id == std::this_thread::get_id());
+	}
 
-    std::thread::id id;
+	std::thread::id thread_id;
 };
 
 #endif
