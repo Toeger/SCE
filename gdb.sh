9944b64cc0ce42d0e01134f5a313b4d4634c3c76
toeger <toeger@anonymouse.me>
2018-07-05 18:39:01 +0200

    Turning off thread creation printing in gdb.

--- gdb.sh
+++ gdb.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -e
-gdb -q --return-child-result -ex "set confirm off" -ex "set pagination off" -ex "handle SIG40 nostop" -ex "run" -ex "bt" -ex "quit" --args $*
+gdb -q --return-child-result -ex "set confirm off" -ex "set pagination off" -ex "handle SIG40 nostop" -ex "set print thread-events off" -ex "run" -ex "bt" -ex "quit" --args $*
