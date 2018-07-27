06442e5443fb700f025d7411a000f7d98734d12d
toeger <toeger@anonymouse.me>
2018-07-27 20:39:49 +0200

    Forgot to add test_mainwindow.h.

---
+++ tests/test_mainwindow.h
@@ -0,0 +1,14 @@
+#ifndef TEST_MAINWINDOW_H
+#define TEST_MAINWINDOW_H
+
+#include "ui/mainwindow.h"
+#include "ui_mainwindow.h"
+
+struct MainWindow_tester : MainWindow {
+	using MainWindow::add_file_tab;
+	using MainWindow::on_file_tabs_tabCloseRequested;
+	using MainWindow::ui;
+	void test();
+} extern *test_main_window;
+
+#endif //TEST_MAINWINDOW_H
