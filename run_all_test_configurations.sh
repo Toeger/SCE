6cc09a06c714bffb23234d3c49c79ebb26a56782
toeger <toeger@anonymouse.me>
2018-09-08 17:29:43 +0200

    Testing running address sanitizer under gdb to reduce compilations.

--- run_all_test_configurations.sh
+++ run_all_test_configurations.sh
@@ -1,10 +1,8 @@
 #!/bin/sh -e
 COMPILERS="CXX=g++
 CXX=clang++"
-BUILDS="BUILD_TYPE=DEBUG SANITIZER=-fsanitize=undefined RUNNER=../gdb.sh
-BUILD_TYPE=DEBUG SANITIZER=-fsanitize=undefined,address
+BUILDS="BUILD_TYPE=DEBUG SANITIZER=-fsanitize=undefined,address
 BUILD_TYPE=DEBUG SANITIZER=-fsanitize=thread
-BUILD_TYPE=RELEASE RUNNER=../gdb.sh
 BUILD_TYPE=RELEASE"
 
 BLUE="\033[0;34m" #blue
@@ -18,7 +16,10 @@ echo "$BUILDS" | while IFS= read -r BUILD; do
 		mkdir -p testbuild && cd testbuild
 		cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -G Ninja .. | grep -v -- "--" || true
 		time ninja SCE_TESTS SCE
-		time $RUNNER ./SCE_TESTS
+		printf "\n===gdb===\n"
+		time ../gdb.sh ./SCE_TESTS
+		printf "\n===native===\n"
+		time ./SCE_TESTS
 		cd .. && rm -R testbuild
 	done
 done
