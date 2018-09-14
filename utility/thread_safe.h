c411ec312022c55327c6462647e8603e91074032
toeger <toeger@anonymouse.me>
2018-09-14 14:12:53 +0200

    Forgot to forward the arguments for Thread_safe.

--- utility/thread_safe.h
+++ utility/thread_safe.h
@@ -5,43 +5,43 @@
 
 template <class T>
 class Thread_safe {
-    struct Thread_safe_wrapper {
-        Thread_safe_wrapper(Thread_safe &parent)
-            : ts{&parent} {
-            ts->m.lock();
-        }
-        Thread_safe_wrapper(const Thread_safe_wrapper &) = delete;
-        ~Thread_safe_wrapper() {
-            ts->m.unlock();
-        }
-        operator T &() {
-            return ts->t;
-        }
-        operator const T &() const {
-            return ts->t;
-        }
-        T &get() {
-            return ts->t;
-        }
-        const T &get() const {
-            return ts->t;
-        }
+	struct Thread_safe_wrapper {
+		Thread_safe_wrapper(Thread_safe &parent)
+			: ts{&parent} {
+			ts->m.lock();
+		}
+		Thread_safe_wrapper(const Thread_safe_wrapper &) = delete;
+		~Thread_safe_wrapper() {
+			ts->m.unlock();
+		}
+		operator T &() {
+			return ts->t;
+		}
+		operator const T &() const {
+			return ts->t;
+		}
+		T &get() {
+			return ts->t;
+		}
+		const T &get() const {
+			return ts->t;
+		}
 
-        private:
-        Thread_safe *ts{};
-    };
+		private:
+		Thread_safe *ts{};
+	};
 
-    public:
-    template <class... Args>
-    Thread_safe(Args &&... args)
-        : t(args...) {}
-    Thread_safe_wrapper lock() {
-        return *this;
-    }
+	public:
+	template <class... Args>
+	Thread_safe(Args &&... args)
+		: t{std::forward<Args>(args)...} {}
+	Thread_safe_wrapper lock() {
+		return *this;
+	}
 
-    private:
-    T t;
-    std::mutex m;
+	private:
+	T t;
+	std::mutex m;
 };
 
 #endif
