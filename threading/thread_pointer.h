bc238f9495067056547ee74074b44727c523d0d5
toeger <toeger@anonymouse.me>
2018-11-11 02:51:01 +0100

    Now comparison compiles under gcc.

--- threading/thread_pointer.h
+++ threading/thread_pointer.h
@@ -38,157 +38,161 @@ struct Thread_pointer {
 	}
 	T *t = nullptr;
 
+	public:
 	template <class U>
-	friend auto operator_spaceship(const Thread_pointer<U> &lhs, const Thread_pointer<U> &rhs) {
+	static auto operator_spaceship(const Thread_pointer<U> &lhs, const Thread_pointer<U> &rhs) {
 		//return std::compare_3way(lhs.t, rhs.t);
 		return std::less<>{}(lhs.t, rhs.t) ? -1 : std::less<>{}(rhs.t, lhs.t) ? 1 : 0;
 	}
 	template <class U>
-	friend auto operator_spaceship(const Thread_pointer<U> &lhs, std::nullptr_t rhs) {
+	static auto operator_spaceship(const Thread_pointer<U> &lhs, std::nullptr_t rhs) {
 		//return std::compare_3way(lhs.t, rhs);
 		return std::less<>{}(lhs.t, rhs) ? -1 : std::less<>{}(rhs, lhs.t) ? 1 : 0;
 	}
 	template <class U>
-	friend auto operator_spaceship(std::nullptr_t lhs, const Thread_pointer<U> &rhs) {
+	static auto operator_spaceship(std::nullptr_t lhs, const Thread_pointer<U> &rhs) {
 		//return std::compare_3way(lhs, rhs.t);
 		return std::less<>{}(lhs, rhs.t) ? -1 : std::less<>{}(rhs.t, lhs) ? 1 : 0;
 	}
 	template <class U>
-	friend auto operator_spaceship(const Thread_pointer<U> &lhs, U *rhs) {
+	static auto operator_spaceship(const Thread_pointer<U> &lhs, U *rhs) {
 		//return std::compare_3way(lhs.t, rhs);
 		return std::less<>{}(lhs.t, rhs) ? -1 : std::less<>{}(rhs, lhs.t) ? 1 : 0;
 	}
 	template <class U>
-	friend auto operator_spaceship(U *lhs, const Thread_pointer<U> &rhs) {
+	static auto operator_spaceship(U *lhs, const Thread_pointer<U> &rhs) {
 		//return std::compare_3way(lhs, rhs.t);
 		return std::less<>{}(lhs, rhs.t) ? -1 : std::less<>{}(rhs.t, lhs) ? 1 : 0;
 	}
 };
 
 template <class T>
+using GUI_ptr = Thread_pointer<T>;
+
+template <class T>
 bool operator==(const Thread_pointer<T> &lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) == 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) == 0;
 }
 template <class T>
 bool operator==(const Thread_pointer<T> &lhs, std::nullptr_t rhs) {
-	return operator_spaceship(lhs, rhs) == 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) == 0;
 }
 template <class T>
 bool operator==(std::nullptr_t lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) == 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) == 0;
 }
 template <class T>
 bool operator==(const Thread_pointer<T> &lhs, T *rhs) {
-	return operator_spaceship(lhs, rhs) == 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) == 0;
 }
 template <class T>
 bool operator==(T *lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) == 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) == 0;
 }
 
 template <class T>
 bool operator!=(const Thread_pointer<T> &lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) != 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) != 0;
 }
 template <class T>
 bool operator!=(const Thread_pointer<T> &lhs, std::nullptr_t rhs) {
-	return operator_spaceship(lhs, rhs) != 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) != 0;
 }
 template <class T>
 bool operator!=(std::nullptr_t lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) != 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) != 0;
 }
 template <class T>
 bool operator!=(const Thread_pointer<T> &lhs, T *rhs) {
-	return operator_spaceship(lhs, rhs) != 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) != 0;
 }
 template <class T>
 bool operator!=(T *lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) != 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) != 0;
 }
 
 template <class T>
 bool operator<(const Thread_pointer<T> &lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) < 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) < 0;
 }
 template <class T>
 bool operator<(const Thread_pointer<T> &lhs, std::nullptr_t rhs) {
-	return operator_spaceship(lhs, rhs) < 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) < 0;
 }
 template <class T>
 bool operator<(std::nullptr_t lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) < 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) < 0;
 }
 template <class T>
 bool operator<(const Thread_pointer<T> &lhs, T *rhs) {
-	return operator_spaceship(lhs, rhs) < 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) < 0;
 }
 template <class T>
 bool operator<(T *lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) < 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) < 0;
 }
 
 template <class T>
 bool operator<=(const Thread_pointer<T> &lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) <= 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) <= 0;
 }
 template <class T>
 bool operator<=(const Thread_pointer<T> &lhs, std::nullptr_t rhs) {
-	return operator_spaceship(lhs, rhs) <= 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) <= 0;
 }
 template <class T>
 bool operator<=(std::nullptr_t lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) <= 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) <= 0;
 }
 template <class T>
 bool operator<=(const Thread_pointer<T> &lhs, T *rhs) {
-	return operator_spaceship(lhs, rhs) <= 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) <= 0;
 }
 template <class T>
 bool operator<=(T *lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) <= 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) <= 0;
 }
 
 template <class T>
 bool operator>(const Thread_pointer<T> &lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) > 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) > 0;
 }
 template <class T>
 bool operator>(const Thread_pointer<T> &lhs, std::nullptr_t rhs) {
-	return operator_spaceship(lhs, rhs) > 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) > 0;
 }
 template <class T>
 bool operator>(std::nullptr_t lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) > 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) > 0;
 }
 template <class T>
 bool operator>(const Thread_pointer<T> &lhs, T *rhs) {
-	return operator_spaceship(lhs, rhs) > 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) > 0;
 }
 template <class T>
 bool operator>(T *lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) > 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) > 0;
 }
 
 template <class T>
 bool operator>=(const Thread_pointer<T> &lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) >= 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) >= 0;
 }
 template <class T>
 bool operator>=(const Thread_pointer<T> &lhs, std::nullptr_t rhs) {
-	return operator_spaceship(lhs, rhs) >= 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) >= 0;
 }
 template <class T>
 bool operator>=(std::nullptr_t lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) >= 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) >= 0;
 }
 template <class T>
 bool operator>=(const Thread_pointer<T> &lhs, T *rhs) {
-	return operator_spaceship(lhs, rhs) >= 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) >= 0;
 }
 template <class T>
 bool operator>=(T *lhs, const Thread_pointer<T> &rhs) {
-	return operator_spaceship(lhs, rhs) >= 0;
+	return Thread_pointer<T>::operator_spaceship(lhs, rhs) >= 0;
 }
 
 #endif //THREAD_POINTER_H
