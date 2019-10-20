52b6ef1a6b4c5518ca031934685a0c530cf461dd
toeger <toeger@anony.mous>
2019-10-20 21:49:18 +0200

    Ignoring SIGABORT to try to not crash in travis anymore.

--- main.cpp
+++ main.cpp
@@ -7,12 +7,11 @@
 #include <sce.pb.h>
 #include <string_view>
 
-static void broken_pipe_signal_handler(int) {
-	//don't do anything in the handler, it just exists so the program doesn't get killed when reading or writing a pipe fails and instead receives an error code
-}
-
 int main(int argc, char *argv[]) {
-	signal(SIGPIPE, &broken_pipe_signal_handler);
+    //don't do anything in the handler, it just exists so the program doesn't get killed when reading or writing a pipe fails and instead receives an error code
+    signal(SIGPIPE, [](int) {});
+    //hopefully disable spurious aborts on travis
+    signal(SIGABRT, [](int) {});
 
 	RAII protobuf_lib{[] { GOOGLE_PROTOBUF_VERIFY_VERSION; }, google::protobuf::ShutdownProtobufLibrary};
 
