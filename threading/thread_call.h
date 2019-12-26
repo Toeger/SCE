b42a4aa72a7791d90364cb38b1a03c0556514ccd
toeger <toeger@anony.mous>
2019-12-26 15:48:07 +0100

    Fixed deadlock in threadcalls.

--- threading/thread_call.h
+++ threading/thread_call.h
@@ -10,6 +10,44 @@
 #include <ui/mainwindow.h>
 
 namespace Utility {
+	template <class T>
+	auto get_future_value_from_gui_thread(std::future<T> &&future) {
+		assert(MainWindow::currently_in_gui_thread());
+		while (future.wait_for(std::chrono::milliseconds{16}) == std::future_status::timeout) {
+			QApplication::processEvents();
+		}
+		return future.get();
+	}
+	template <class T>
+	auto get_future_value_from_gui_thread(std::future<T> &&future, const std::chrono::milliseconds timeout) {
+		assert(MainWindow::currently_in_gui_thread());
+		auto start = std::chrono::high_resolution_clock::now();
+		while (future.wait_for(std::chrono::milliseconds{16}) == std::future_status::timeout) {
+			if (std::chrono::high_resolution_clock::now() - start > timeout) {
+				throw std::runtime_error{"timeout"};
+			}
+			QApplication::processEvents();
+		}
+		return future.get();
+	}
+	template <class T>
+	auto get_future_value(std::future<T> &&future) {
+		if (MainWindow::currently_in_gui_thread()) {
+			return get_future_value_from_gui_thread(std::move(future));
+		}
+		return future.get();
+	}
+	template <class T>
+	auto get_future_value(std::future<T> &&future, const std::chrono::milliseconds timeout) {
+		if (MainWindow::currently_in_gui_thread()) {
+			return get_future_value_from_gui_thread(std::move(future), timeout);
+		}
+		if (future.wait_for(timeout) == std::future_status::timeout) {
+			throw std::runtime_error{"timeout"};
+		}
+		return future.get();
+	}
+
 	//this function executes a function fun owned by the owning thread of object
 	template <class Function>
 	void async_owner_thread_execute(QObject *object, Function &&function) {
@@ -49,46 +87,8 @@ namespace Utility {
 				promise.set_value(std::apply(function, std::move(tuple_args)));
 			}
 		});
-		return future.get();
+		return get_future_value(std::move(future));
 	}
-	template <class T>
-	auto get_future_value_from_gui_thread(std::future<T> &&future) {
-		assert(MainWindow::currently_in_gui_thread());
-		while (future.wait_for(std::chrono::milliseconds{16}) == std::future_status::timeout) {
-			QApplication::processEvents();
-		}
-		return future.get();
-	}
-	template <class T>
-	auto get_future_value_from_gui_thread(std::future<T> &&future, const std::chrono::milliseconds timeout) {
-		assert(MainWindow::currently_in_gui_thread());
-		auto start = std::chrono::high_resolution_clock::now();
-		while (future.wait_for(std::chrono::milliseconds{16}) == std::future_status::timeout) {
-			if (std::chrono::high_resolution_clock::now() - start > timeout) {
-				throw std::runtime_error{"timeout"};
-			}
-			QApplication::processEvents();
-		}
-		return future.get();
-	}
-	template <class T>
-	auto get_future_value(std::future<T> &&future) {
-		if (MainWindow::currently_in_gui_thread()) {
-			return get_future_value_from_gui_thread(std::move(future));
-		}
-		return future.get();
-	}
-	template <class T>
-	auto get_future_value(std::future<T> &&future, const std::chrono::milliseconds timeout) {
-		if (MainWindow::currently_in_gui_thread()) {
-			return get_future_value_from_gui_thread(std::move(future), timeout);
-		}
-		if (future.wait_for(timeout) == std::future_status::timeout) {
-			throw std::runtime_error{"timeout"};
-		}
-		return future.get();
-	}
-
 	template <class T>
 	struct Thread_caller {
 		Thread_caller(T *qt_object)
